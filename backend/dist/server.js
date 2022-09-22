70e116503dca1154f939d203fe2dbd329d3afa7c
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-22 13:52:34 +0200

    origin with https://main.d3mfuxjxgnbn1p.amplifyapp.com/

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -37,13 +37,12 @@ const startServer = () => {
     app.use(express_1.default.json());
     // Rules for the API
     app.use((req, res, next) => {
-        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Origin', 'https://main.d3mfuxjxgnbn1p.amplifyapp.com/');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        // if (req.method == 'OPTIONS') {
-        //     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-        //     return res.status(200).json({});
-        // }
+        if (req.method == 'OPTIONS') {
+            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+            return res.status(200).json({});
+        }
         next();
     });
     // Routes
