9d37ddae2e29277ec9a7984a988d7461dcd6533a
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-22 13:28:49 +0200

    trying something with methods line 41 server.ts

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -39,10 +39,11 @@ const startServer = () => {
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-        if (req.method == 'Options') {
-            res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-            return res.status(200).json({});
-        }
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        // if (req.method == 'OPTIONS') {
+        //     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        //     return res.status(200).json({});
+        // }
         next();
     });
     // Routes
