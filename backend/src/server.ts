eb61512aba95bcd9dae9ddff8a2ee0f64c89485b
Sandra Gufler <97165184+kindofclever@users.noreply.github.com>
2022-09-23 21:39:38 +0200

    fsd

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -44,7 +44,7 @@ const startServer = () => {
   app.use(express.json());
     // Rules for the API
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['https://main.d3mfuxjxgnbn1p.amplifyapp.com/', 'http://localhost:3000']);
+    res.header('Access-Control-Allow-Origin', 'https://main.d3mfuxjxgnbn1p.amplifyapp.com/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method == 'Options') {
       res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
